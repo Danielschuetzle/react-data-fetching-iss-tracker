d97e5652e96e0cb4f039e7e75c122a35b8f85ddf
Daniel Schuetzle <102250825+Danielschuetzle@users.noreply.github.com>
2023-06-01 16:51:40 +0200

    comments added

--- components/ISSTracker/index.js
+++ components/ISSTracker/index.js
@@ -1,44 +1,50 @@
-import { useEffect, useState } from "react";
+// Import necessary dependencies
+import useSWR from "swr";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
 
+// Define the API endpoint
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
-export default function ISSTracker() {
-  const [coords, setCoords] = useState({
-    longitude: 0,
-    latitude: 0,
-  });
+// Define the fetcher function to use with SWR
+// It will handle the actual data fetching
+const fetcher = async (url) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    error.status = response.status;
+    throw error;
+  }
+  return response.json();
+};
 
-  async function getISSCoords() {
-    try {
-      const response = await fetch(URL);
-      if (response.ok) {
-        const data = await response.json();
-        setCoords({ longitude: data.longitude, latitude: data.latitude });
-      }
-    } catch (error) {
-      console.error(error);
-    }
+// Define the main component
+export default function ISSTracker() {
+  // Use the SWR hook to fetch data from the API
+  // This will return the fetched data, any error that occurred, and a mutate function
+  // We set the refreshInterval to 5000ms (5 seconds)
+  const { data, error, mutate } = useSWR(URL, fetcher, { refreshInterval: 5000 });
+
+  // If there was an error fetching the data, we display an error message
+  if (error) {
+    return <div>Error: {error.message}</div>;
   }
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      getISSCoords();
-    }, 5000);
+  // If the data is still loading (i.e., undefined), we display a loading message
+  if (!data) {
+    return <div>Loading...</div>;
+  }
 
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
+  // Once the data is available, we render the Map and Controls components
+  // We pass the fetched longitude and latitude as props to both components
+  // We pass the mutate function (which refetches the data) as the onRefresh prop to the Controls component
   return (
     <main>
-      <Map longitude={coords.longitude} latitude={coords.latitude} />
+      <Map longitude={data.longitude} latitude={data.latitude} />
       <Controls
-        longitude={coords.longitude}
-        latitude={coords.latitude}
-        onRefresh={getISSCoords}
+        longitude={data.longitude}
+        latitude={data.latitude}
+        onRefresh={() => mutate()}  // refetch data when Refresh button is clicked
       />
     </main>
   );
